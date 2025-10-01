682981255b63d27758a223be0e0d476813ab57c5
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-10-01 23:16:12 +0100

    Update _app.tsx

--- restore-image/src/pages/_app.tsx
+++ restore-image/src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { supabase } from '@/lib/supabase';
 import type { AppProps } from 'next/app';
+import { SupabaseProvider } from '@supabase/ssr';
+import { supabase } from '@/lib/supabase';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionContextProvider supabaseClient={supabase}>
+    <SupabaseProvider supabaseClient={supabase}>
       <Component {...pageProps} />
-    </SessionContextProvider>
+    </SupabaseProvider>
   );
 }
