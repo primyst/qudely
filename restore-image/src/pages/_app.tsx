6c81aa42b893195540d30dd91033212547e4b320
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-10-01 22:21:25 +0100

    Update _app.tsx

--- restore-image/src/pages/_app.tsx
+++ restore-image/src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { supabase } from '@/lib/supabase';
+import type { AppProps } from 'next/app';
 
-export default function MyApp({ Component, pageProps }: any) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionContextProvider supabaseClient={supabase}>
       <Component {...pageProps} />
