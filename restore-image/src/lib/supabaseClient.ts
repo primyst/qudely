d44bb42c61d25b97c0f50d89155d3f19e582f7d2
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-10-04 18:39:50 +0100

    Update supabaseClient.ts

--- restore-image/src/lib/supabaseClient.ts
+++ restore-image/src/lib/supabaseClient.ts
@@ -1,9 +1,7 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { SupabaseClient } from "@supabase/supabase-js";
-import { Database } from "@/types/supabase";
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "@/types/supabase";
 
-export const createClient = (): SupabaseClient<Database> =>
-  createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+export const supabase = createClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
