292bf8e7f51192e88a0121f46b9b94bb46535bbd
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-10-10 11:48:21 +0100

    Update supabaseClient.ts

--- restore-image/src/lib/supabaseClient.ts
+++ restore-image/src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+// Pull your environment variables from Next.js
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
